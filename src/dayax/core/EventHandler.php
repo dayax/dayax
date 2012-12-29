f246a206317b09cbc5188b388395ee1130d9b6e6
Anthonius Munthi <toni.munthi@gmail.com>
2012-12-29 12:36:06 +0800

    added new EventHandler class

---
+++ src/dayax/core/EventHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the dayax project.
+ * 
+ * (c) Anthonius Munthi <me@itstoni.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace dayax\component;
+
+class EventHandler
+{
+    public function add($name,$handler,$priority=null)
+    {
+        
+    }
+    
+    private function sortHandler()
+    {
+        
+    }
+}
